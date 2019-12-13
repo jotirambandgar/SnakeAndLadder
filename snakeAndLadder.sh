8bb67378affc2d21fa7e52a8d17d789f51b6f6e0
jotirambandgar <bandgar09@gmail.com>
2019-12-13 18:29:39 +0530

    Added : Option for playing game

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,4 +1,30 @@
 #! /bin/bash -x
 
+NO_PLAY=0
+LADDER=1
+SNAKE=2
+
 playerPosition=0
-echo $(( RANDOM % 6 + 1))
+
+function startPlaying(){
+	while [ $playerPosition -ne 100 ]
+	do
+		die=$(( RANDOM % 6 + 1))
+		option=$((RANDOM%3))
+		echo "$option"
+		case $option in
+			$NO_PLAY)
+					echo "no play" ;;
+			$LADDER)
+					playerPosition=$(( $playerPosition + $die )) ;;
+			$SNAKE)
+				if [ $die -lt $playerPosition ]
+				then
+				 	playerPosition=$(( $playerPosition - $die )) 
+				fi;;
+		esac
+	done
+
+}
+
+startPlaying
