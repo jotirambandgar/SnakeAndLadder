aef10df0086a2b0dc4592f6cc6ed42c2c19a85ca
jotirambandgar <bandgar09@gmail.com>
2019-12-14 15:01:28 +0530

    modified : winning functionality changed to exact 100 position

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -3,24 +3,26 @@
 NO_PLAY=0
 LADDER=1
 SNAKE=2
-
 playerPosition=0
 
 function startPlaying(){
-	while [ $playerPosition -le 100 ]
+	while [ $playerPosition -lt 100 ]
 	do
-		die=$(( RANDOM % 6 + 1))
-		option=$((RANDOM%3))
+		local die=$(( RANDOM % 6 + 1))
+		local option=$((RANDOM%3))
 
 		case $option in
 			$NO_PLAY)
 					echo "no play" ;;
 			$LADDER)
-					playerPosition=$(( $playerPosition + $die )) ;;
+					if [ $(($playerPosition + $die)) -le 100 ]
+					then
+						playerPosition=$(( $playerPosition + $die ))
+					fi ;;
 			$SNAKE)
 					if [ $die -lt $playerPosition ]
 					then
-				 		playerPosition=$(( $playerPosition - $die )) 
+				 		playerPosition=$(( $playerPosition - $die ))
 					fi ;;
 		esac
 	done
