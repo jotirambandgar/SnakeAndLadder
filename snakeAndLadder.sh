10fd0052ab7408384a6872d443a0ab80e5ce2383
jotirambandgar <bandgar09@gmail.com>
2019-12-14 12:18:02 +0530

    Added : winning position for game

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -7,24 +7,24 @@ SNAKE=2
 playerPosition=0
 
 function startPlaying(){
-	while [ $playerPosition -ne 100 ]
+	while [ $playerPosition -le 100 ]
 	do
 		die=$(( RANDOM % 6 + 1))
 		option=$((RANDOM%3))
-		echo "$option"
+
 		case $option in
 			$NO_PLAY)
 					echo "no play" ;;
 			$LADDER)
 					playerPosition=$(( $playerPosition + $die )) ;;
 			$SNAKE)
-				if [ $die -lt $playerPosition ]
-				then
-				 	playerPosition=$(( $playerPosition - $die )) 
-				fi;;
+					if [ $die -lt $playerPosition ]
+					then
+				 		playerPosition=$(( $playerPosition - $die )) 
+					fi ;;
 		esac
 	done
-
+	echo "$playerPosition"
 }
-
 startPlaying
+echo "$playerPosition"
